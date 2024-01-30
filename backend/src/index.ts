0bda514c89556dc8d2cab0add3403d3bd3b72d6e
LoubatTh <thomasloubat1@gmail.com>
2024-01-30 12:01:31 +0100

    feat: connect front and back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import { Request, Response, response } from "express";
+
 const express = require('express');
 const cors = require('cors');
 
@@ -5,5 +7,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
+app.use("/hello", (req: Request, res: Response) => {
+    res.send({message: "Hello World!"});
+});
 
 app.listen(5000, () => console.log('Server running on port 5000'));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-function App() {
+import { useState, useEffect } from "react";
 
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  })
   return (
     <>
       <h1 className=" text-red-900">React App</h1>
+      <p>{message}</p>
     </>
   )
 }
