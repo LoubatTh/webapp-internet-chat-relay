da726f415f7457851fd3f5827e02a0933d56b923
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-02-06 19:04:46 +0100

    feat: add event on enter key

--- frontend/src/components/bodyComponents/InputMessage.tsx
+++ frontend/src/components/bodyComponents/InputMessage.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/ui/button";
 import { Textarea } from "../ui/ui/textarea";
 
 const InputMessage = () => {
+  const [message, setMessage] = useState<string>("");
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault(); 
+      sendMessage();
+    }
+  };
+
+  const sendMessage = () => {
+    setMessage("");
+    console.log("Sending message:", message);
+  };
+
   return (
     <div className="flex flex-row h-5 w-full p-2 gap-2">
       <Textarea
         className="resize-none text-secondary bg-white rounded-md w-full p-1 min-h-10"
         placeholder="Type your message here."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
-      <Button className="h-10">Send</Button>
+      <Button className="h-10" onClick={sendMessage}>
+        Send
+      </Button>
     </div>
   );
 };
