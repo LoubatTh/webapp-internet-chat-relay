cddd17df2f55fce3dd6aaef4f392de490fe92e1b
matteo degano <matteo.degano@epitech.eu>
2024-02-13 10:01:27 +0100

    addType

--- frontend/src/lib/type.ts
+++ frontend/src/lib/type.ts
@@ -1,3 +1,48 @@
+export type ServerType = {
+  _id: string
+  name: string
+  channels: Channels[]
+  members: Members[]
+};
+
+export type Channels = {
+  cid: number
+};
+
+export type Members = {
+  uid: number
+  role: string
+};
+
+export type UserType = {
+  _id: string
+  username: string
+  informations: string
+  channels: ChannelType[],
+  createdAt: string
+};
+
+export type UserPostType = {
+  username: string
+  informations: string
+  channels: ChannelType[],
+  createdAt: string
+};
+
+export type GuestType = {
+  _id: string
+  username: string
+  channels: ChannelType[]
+  lastConnexion: string
+}
+
+export type GuestPostType = {
+  _id: string
+  username: string
+  channels: ChannelType[]
+  lastConnexion: string
+}
+
 export type ChannelType = {
   _id: string;
   name: string;
@@ -5,16 +50,25 @@ export type ChannelType = {
   members: string[];
 };
 
+export type ChannelPostType = {
+  name: string;
+  visibility: string;
+  members: string[];
+  guests: string[];
+};
+
 export type MessagesType = {
   _id: string;
   text: string | JSX.Element;
   channelId: string;
-  author: string;
+  authorId: string[];
+  createdAt: string
 };
 
 export type MessagesPostType = {
-  text: string;
+  text: string | JSX.Element;
   channelId: string;
-  author: string;
+  authorId: string[];
+  createdAt: string
 };
 
