5f2c38b300f07480c45550fc0ded89b54e6e2a6d
LoubatTh <thomas.loubat@epitech.eu>
2024-02-20 16:54:41 +0100

    feat(user): auth middleware

--- backend/src/routes/channels.routes.ts
+++ backend/src/routes/channels.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import * as channelsController from "../controllers/channels.controller";
+import { checkJwtToken } from "~/middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/", channelsController.getChannels);
 router.get("/:id", channelsController.getChannel);
-router.post("/", channelsController.createChannel);
-router.put("/:id", channelsController.updateChannel);
-router.delete("/:id", channelsController.deleteChannel);
+router.post("/", checkJwtToken, channelsController.createChannel);
+router.put("/:id", checkJwtToken, channelsController.updateChannel);
+router.delete("/:id", checkJwtToken, channelsController.deleteChannel);
 
 module.exports = router;
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as usersController from "../controllers/users.controller";
 import * as authController from "../controllers/auth.controller";
+import { checkJwtToken } from "~/middlewares/auth.middleware";
 
 const router = express.Router();
 
@@ -8,10 +9,10 @@ router.post("/login", authController.login);
 router.post("/register", authController.register);
 router.get("/", usersController.getUsers);
 router.get("/:id", usersController.getUser);
-router.put("/:id", usersController.updateUser);
-router.delete("/:id", usersController.deleteUser);
-router.get("/:id/channels", usersController.getUserChannels);
-router.post("/:id/channels", usersController.addUserChannel);
-router.delete("/:id/channels/:channelId", usersController.removeUserChannel);
+router.put("/:id", checkJwtToken, usersController.updateUser);
+router.delete("/:id", checkJwtToken, usersController.deleteUser);
+router.get("/:id/channels", checkJwtToken, usersController.getUserChannels);
+router.post("/:id/channels", checkJwtToken, usersController.addUserChannel);
+router.delete("/:id/channels/:channelId", checkJwtToken, usersController.removeUserChannel);
 
 module.exports = router;
